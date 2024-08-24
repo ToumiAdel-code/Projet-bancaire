57768b604720defa1c485e8156097b0d8c35b6ff
Adeltoumi-Code <adel.toumi@esprit.tn>
2024-08-24 12:49:10 +0100

    create modele

---
+++ src/main/java/com/adel/banking/models/Account.java
@@ -0,0 +1,20 @@
+package com.adel.banking.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue
+    private Integer id;
+}
---
+++ src/main/java/com/adel/banking/models/User.java
@@ -0,0 +1,30 @@
+package com.adel.banking.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private  String password;
+}
